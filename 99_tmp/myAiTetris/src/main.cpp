e3733ce3848100fdb7a3ab4549edee7b9b4d00be
marco <marco_hmc@163.com>
2024-04-25 22:01:20 +0800

    template

---
+++ 99_tmp/myAiTetris/src/main.cpp
@@ -0,0 +1,10 @@
+#include "ui/widget.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char* argv[]) 
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    return a.exec();
+}
