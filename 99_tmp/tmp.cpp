50c5f0f553d6ada30a3150258cb84fcb8436a2b5
marco <marco_hmc@163.com>
2024-10-29 18:05:54 +0800

    threads

--- 99_tmp/tmp.cpp
+++ 99_tmp/tmp.cpp
@@ -1,34 +1,112 @@
-std::mutex mtx;
-std::condition_variable cond;
-std::deque<int> queue;
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <vector>
 
-int dequeue() {
-    std::unique_lock<std::mutex> lock(mtx);
-    cond.wait(lock,
-              [] { return !queue.empty(); });  // 使用 lambda 表达式检查条件
-    int top = queue.front();
-    queue.pop_front();
-    return top;
-}
+class ThreadPool {
+   public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
 
-int dequeue2() {
-    // queue pop掉最后一个,并返回
-    MutexLockGuard lock(mutex);
-    while (queue.empty()) {  // 不能用if,必须用while,避免虚假唤醒
-        cond.wait();
+    template <class F, class... Args>
+    auto enqueue(F&& f, Args&&... args)
+        -> std::future<typename std::result_of<F(Args...)>::type>;
+
+   private:
+    void workerThread();
+
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    bool stop;
+};
+
+ThreadPool::ThreadPool(size_t numThreads) : stop(false) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers.emplace_back(&ThreadPool::workerThread, this);
     }
-    int top = queue.front();
-    queue.pop_front();
-    return top;
 }
 
-  int dequeue3() {
-      // queue pop掉最后一个,并返回
-      MutexLockGuard lock(mutex);
-      while (queue.empty()) {  // 不能用if,必须用while,避免虚假唤醒
-          cond.wait();
-      }
-      int top = queue.front();
-      queue.pop_front();
-      return top;
-  }
+ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
+
+template <class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+    -> std::future<typename std::result_of<F(Args...)>::type> {
+    using returnType = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<returnType()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+    std::future<returnType> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queueMutex);
+        if (stop) {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+        tasks.emplace([task]() { (*task)(); });
+    }
+    condition.notify_one();
+    return res;
+}
+
+void ThreadPool::workerThread() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            condition.wait(lock, [this] { return stop || !tasks.empty(); });
+            if (stop && tasks.empty()) {
+                return;
+            }
+            task = std::move(tasks.front());
+            tasks.pop();
+        }
+        task();
+    }
+}
+
+void benchmarkTask() {
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = start + std::chrono::seconds(3);
+    long long counter = 0;
+    while (std::chrono::high_resolution_clock::now() < end) {
+        ++counter;
+    }
+    std::cout << "Counter: " << counter / 1e8 << "\n\n";
+}
+
+int main() {
+    {
+        std::thread t1(benchmarkTask);
+        std::thread t2(benchmarkTask);
+
+        t1.join();
+        t2.join();
+    }
+
+    ThreadPool pool(1024);  // 创建一个包含24个线程的线程池
+
+    {
+        std::thread t1(benchmarkTask);
+        std::thread t2(benchmarkTask);
+
+        t1.join();
+        t2.join();
+    }
+    return 0;
+}
